d6cfd8de36af8e130cc3e60222440517a23274c9
João Laino <joaolainn@gmail.com>
2025-05-08 19:14:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import requests
-import pandas as pd
-import matplotlib.pyplot as plt
+# Importando bibliotecas necessárias
+import requests  # Para fazer requisições HTTP
+import pandas as pd  # Para manipulação de dados em DataFrame
+import matplotlib.pyplot as plt  # Para visualização gráfica
 
+# Requisitando dados da API de COVID-19 no Brasil
 url = "https://covid19-brazil-api.now.sh/api/report/v1"
-resposta = requests.get(url)
-dados = resposta.json()
+resposta = requests.get(url)  # Obtendo a resposta da API
+dados = resposta.json()  # Convertendo a resposta para JSON
+
+# Criando um DataFrame com os dados de COVID-19
 tabela = pd.DataFrame(dados['data'])
 
+# Selecionando as 5 primeiras linhas com informações dos estados, mortes e casos
 tabela_areas = pd.DataFrame({
     'Estados': tabela['uf'].head(),
     'Mortes': tabela["deaths"].head(),
@@ -14,34 +19,26 @@ tabela_areas = pd.DataFrame({
 })
 print(tabela_areas)
 
+# Realizando uma junção entre o DataFrame original e o tabela_areas com base nas mortes
 tabela = tabela.merge(tabela_areas, how="left", left_on="deaths", right_on="Mortes")
-#tabela = tabela.drop(columns="Nome Funcionário")
 
+# Configurando a janela do gráfico
 fig = plt.figure()
 fig.canvas.manager.set_window_title("Análise Casos de COVID por Estado (UF)")
 
+# Agrupando e somando os casos e mortes por estado
 resumo_uf = tabela.groupby("Estados")[["Casos", "Mortes"]].sum().sort_values("Casos", ascending=False)
+
+# Criando gráfico de barras para mostrar o total de casos
 cores = ['#007acc', '#33a02c', '#ff7f00', '#6a3d9a', '#e31a1c']
 resumo_uf["Casos"].plot(kind="bar", color=cores, figsize=(10,6))
-#plt.plot(tabela["Casos"], tabela["Mortes"], color='blue', linestyle='--', marker='o')
 
+# Adicionando título, rótulos e texto ao gráfico
 plt.title("Total de Casos por Estado (UF)")
 plt.xlabel("UF")
 plt.ylabel("Casos")
 plt.text(x=tabela["Casos"][6], y=160000, s="Pico de casos", fontsize=10, color="red")
 plt.grid(True)
 
+# Exibindo o gráfico
 plt.show()
-
-#ver_estado = input("Digite qual Estado você deseja ver o gráfico: ")
-#if ver_estado == "São Paulo":
-# Criando gráfico de barras com os dados totais por UF
-    #sp = tabela["Estados"][0]
-    #resumo_uf = tabela.groupby(sp)[["Casos", "Mortes"]].sum().sort_values("Casos", ascending=False)
-    #resumo_uf["Casos"].plot(kind="bar", figsize=(12,6), color="skyblue")
-    #plt.title("Total de Casos por Estado (UF)")
-    #plt.xlabel("UF")
-    #plt.ylabel("Total de Casos")
-    #plt.grid(True)
-    #plt.show()
-
